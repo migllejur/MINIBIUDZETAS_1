eaaee7c9cafe505a804305793abb5ea4459da207
migllejur <migllejur@gmail.com>
2024-11-29 10:27:53 +0200

    pridetas atspaudinimu kodas + pakeitimai
    patobulintas ivesciu kodas, pridetas q ivesties kodas baigti programai

--- minibiudzetas_1.py
+++ minibiudzetas_1.py
@@ -16,12 +16,20 @@ while True:
         datos_formatas = datetime.datetime.strptime(data, "%Y %m %d")
         pajamu_pav = input("Pajamų pavadinimas: ")
         suma = float(input("Pajamų suma: "))
-        pajamu_listas = [datos_formatas, pajamu_pav, suma]
+        pajamu_listas = [datos_formatas.strftime("%Y %m %d"), pajamu_pav, suma]
         pajamos.append(pajamu_listas)
     if ivestis == "2":
         data = input("Data: ")
         datos_formatas = datetime.datetime.strptime(data, "%Y %m %d")
         islaidu_pav = input("Išlaidų pavadinimas: ")
         suma = float(input("Išlaidų suma: "))
-        islaidu_listas = [datos_formatas, islaidu_pav, suma]
+        islaidu_listas = [datos_formatas.strftime("%Y %m %d"), islaidu_pav, suma]
         islaidos.append(islaidu_listas)
+    if ivestis == "3":
+        for elem in pajamos:
+            print(elem)
+    if ivestis == "4":
+        for elem in islaidos:
+            print(elem)
+    if ivestis == "q":
+        break
