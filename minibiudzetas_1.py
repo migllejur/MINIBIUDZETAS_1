e409d83a21efcfdb3136c980c324e8e1978a9c96
migllejur <migllejur@gmail.com>
2024-11-29 09:48:21 +0200

    sukurti tusti listai ir meniu

---
+++ minibiudzetas_1.py
@@ -0,0 +1,10 @@
+pajamos = []
+islaidos = []
+
+while True:
+    print("1. Įvesti pajamas\n"
+          "2. Įvesti išlaidas\n"
+          "3. Atspausdinti pajamų eilutes\n"
+          "4. Atspausdinti išlaidų eilutes\n"
+          "5. Atspausdinti statistiką\n"
+          "q - išeiti")
