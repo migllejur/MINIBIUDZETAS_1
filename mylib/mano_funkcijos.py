df75d3ca1101e3dfbc5c60d56e957d1a7b391812
migllejur <migllejur@gmail.com>
2024-11-29 12:56:07 +0200

    sukurta istrynimo ir ieskojimo funkcija

--- mylib/mano_funkcijos.py
+++ mylib/mano_funkcijos.py
@@ -8,3 +8,24 @@ def statistika(*args, operacija="suma"):
     elif operacija == "average":
         rezultatas = sum(args) / len(args)
     return rezultatas
+
+
+def istrinti_ieskoti(*args, operacija="istrinti"):
+    if operacija == "istrinti":
+        for nr, elem in enumerate(args, start=1):
+            print(f"{nr}. {elem}")
+        try:
+            sk = int(input("Įveskite skaičių duomenų, kuriuos norite ištrinti: "))
+            if sk <= len(args):
+                del args[sk - 1]
+                print("Duomenys ištrinti")
+        except ValueError:
+            print("Klaida. Įvestas netinkamas skaičius. Bandykite dar kartą.")
+    if operacija == "ieskoti":
+        search = input("Įveskite raktažodį (sumą, pavadinimą arba datą): ")
+        for elem in args:
+            if search in elem:
+                print(elem)
+        else:
+            print("Duomenys nerasti")
+    return args
